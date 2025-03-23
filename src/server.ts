acb99e4cc21a928f62cd22fedc57dc7502d0d4c2
Dalala <nik.dalalishvili@gmail.com>
2025-03-23 15:27:59 +0100

    implement schemas to db

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
+import http from "http";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 export function startServer() {
-    console.log("Server is running...");
+    const server = http.createServer();
+
+    server.listen(PORT, () => {
+        console.log("server is running");
+    });
 }
